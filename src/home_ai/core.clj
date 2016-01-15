e11a4471b9281f70495b4c7898f4cbebaca8456f
bemcho <bemcho@gmail.com>
2016-01-15 16:06:57 +0200

    Clean up

--- src/home_ai/core.clj
+++ src/home_ai/core.clj
@@ -14,13 +14,6 @@
     (start-visual-repl)
     ; A button with an action listener that will cause the dialog to close
     ; and return :ok to the invoker.
-    (comment
-      (.setLabelInfo @lbph-face-recognizer 1 "Emil Tomov")
-      (.setLabelInfo @lbph-face-recognizer 0 "Unknown")
-      (.setLabelInfo @lbph-face-recognizer 3 "Venceslav Marinov")
-      (.setLabelInfo @lbph-face-recognizer 2 "Deyan Rizov")
-      (.setLabelInfo @lbph-face-recognizer 4 "Svetoslav Kolev"))
-    )
     )
 
 
